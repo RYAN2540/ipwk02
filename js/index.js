4d5e167d08148906745e2619b73bdd56a4f9bea5
RYAN2540 <austinbrian005@gmail.com>
2020-04-03 18:03:17 +0300

    validation

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const init = function(){
+    document.getElementById("button-cancel").addEventListener("click",reset);
+    document.getElementById("buttton-submit").addEventListener("click",send);
+}
+const reset = function(ev){
+    ev.preventDefault();
+    document.getElementById("form-user").reset();
+}
+const submit = function(ev){
+    ev.preventDefault();
+    let ret = validate();
+    if(ret){
+       document.getElementById("form-user").submit(); 
+    }else{
+        let err = documnet.querySelector(".error");
+        let input = err.querySelector("input");
+        err.setAtrribute("data errormsg", ` ...Missing ${input.placeholder}`);
+    }  
+
+}
+const validate = function(ev){
+    let valid = false;
+    let chk = document.getElementById("gender")
+    let birthday  = document.getElementById("birthday");
+}
+
+
+
+
+
+
+var dayOfBirth = document.getElementById("birthday").value;
+var dateEntered = dayOfBirth.split("birthday");
+var day = parseInt(dateEntered[2]);
+var month =parseInt(dateEntered[1]);
+var year = parseInt|(dateEntered[0]);
